30ee508eafc3539f1ab79ad342a47dfc8e2bf74f
navi <ivankurban1986@gmail.com>
2023-09-22 10:35:28 +0300

    test in comment

--- client_methods.py
+++ client_methods.py
@@ -6,11 +6,9 @@ def get_dataset(dataset_path: str, with_split: bool, test_size: float, shuffle: 
     pass
 
 
-# def train(model: torch.nn.Module, train_set: torch.utils.data.Dataset, epochs, batch_size, lr,
-#           valid_set: torch.utils.data.Dataset = None) -> Tuple[List[Metric], torch.nn.Module]:
-#     pass
+def train(model: torch.nn.Module, train_set: torch.utils.data.Dataset, epochs, batch_size, lr, valid_set: torch.utils.data.Dataset = None):
+    pass
 
 
-# def test(model: torch.nn.Module, test_set: torch.utils.data.Dataset, return_output: bool) -> Union[
-#     Tuple[List[Metric]], Tuple[List[Metric], list]]:
-#     pass
+def test(model: torch.nn.Module, test_set: torch.utils.data.Dataset, return_output: bool):
+    pass
